55d2a573722833cf65d981f70d362091aa161be4
dev <alexmartos96@gmail.com>
2020-03-05 23:13:33 -0800

    got dynamic route working

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <h1>I am a blog post</h1>
+    <h1>I am a blog post {{id}}</h1>
   </div>
 </template>
 <script>
 export default {
+  created() {
+    console.log(this.$route.params.id);
+    this.id = this.$route.params.id;
+  },
   data: () => ({
-    id: this.$route.params.id
+    id: null
   })
 };
 </script>
